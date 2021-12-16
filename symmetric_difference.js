af5bc886f76afdd8ce7a40a154518bab8bf90cba
Ezzin Houssam <ezzinhoussam@gmail.com>
2021-12-16 10:00:01 +0100

    update!

--- symmetric_difference.js
+++ symmetric_difference.js
@@ -1,19 +1,55 @@
-function sym(...args) {
-
-
-    for(let i = 0; i< Math.max(args[0].length, args[1].length);i++){
-        console.log("gg",i);
-        if(args[1].includes(args[0][i])){
-            args[1].splice(args[1].indexOf(args[0][i]),1);
-        }else{
-            args[1].push(args[0][i])
-            args[0].splice(i,1)
-        }
+function flatArray(array){
+  let result = []
+  for(a in array){
+    for(i in a){
+      result.push(parseInt(a))
     }
-
-    
-    uniq = [...new Set(args[1].reverse())];
-    console.log("resultddd : ", uniq);
-    return uniq;
   }
-  sym([1, 2, 3], [5, 2, 1, 4])
+  return result
+}
+
+function checkForOcc(element,index,arr){
+  return element === arr[index]
+}
+
+function sym(...args) {
+    /*
+    const array1 = [1, 2, 3, 4];
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+console.log(array1.reduce(reducer));
+
+    */
+    let all = []
+    for(i in args){
+      all.push(args[i])
+    }
+    console.log(all);
+    all = flatArray(all);
+    let l = []
+    for(elem in all){
+      // console.log(elem)
+      if(checkForOcc(all[elem],elem,all)){
+        l.push(parseInt(elem));
+      }
+    }
+    return l;
+    
+  }
+
+console.log("Result 1 :", sym([1, 2, 5], [2, 3, 5], [3, 4, 5]))
+console.log("Result 2 : ",sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) )
+console.log("Result 3 :",sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]))
+
+  /*
+sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
+sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
+// tests completed
+
+  */
+
+console.log("Result : ",sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]))  
